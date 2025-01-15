7e530312fc8ef5616bf49be4217f33c1b8371ceb
monkeytickler812 <159182493+monkeytickler812@users.noreply.github.com>
2025-01-15 11:28:47 -0800

    fixed L4 movement

--- M10.twin_stick_workbook/player/player.gd
+++ M10.twin_stick_workbook/player/player.gd
@@ -1,51 +1,37 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
-const GODOT_BOTTOM = preload("res://player/godot_bottom.png")
-const GODOT_BOTTOM_RIGHT = preload("res://player/godot_bottom_right.png")
-const GODOT_RIGHT = preload("res://player/godot_right.png")
-const GODOT_UP = preload("res://player/godot_up.png")
-const GODOT_UP_RIGHT = preload("res://player/godot_up_right.png")
+@export var speed := 460.0
+@export var drag_factor := 10.0
+
+const SPRITE_RIGHT := preload("res://player/godot_right.png")
+const SPRITE_DOWN := preload("res://player/godot_bottom.png")
+const SPRITE_UP := preload("res://player/godot_up.png")
+const SPRITE_DOWN_RIGHT := preload("res://player/godot_bottom_right.png")
+const SPRITE_UP_RIGHT := preload("res://player/godot_up_right.png")
 const UP_RIGHT = Vector2.UP + Vector2.RIGHT
 const UP_LEFT = Vector2.UP + Vector2.LEFT
 const DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
 const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
 
-@export var SPEED = 300.0
-@export var acceleration := 1200.0
-@export var deceleration := 1080.0
-const JUMP_VELOCITY = -400.0
-
 @onready var _skin: Sprite2D = %Skin
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction * SPEED
-	move_and_slide()
-
-	var direction_discrete := direction.sign()
+func _physics_process(delta: float) -> void:
+	var move_direction := Input.get_vector("left", "right", "up", "down")
+	var desired_velocity := speed * move_direction
+	var steering := desired_velocity - velocity
+	velocity += steering * drag_factor * delta
+	var direction_discrete := move_direction.sign()
 	match direction_discrete:
 		Vector2.RIGHT, Vector2.LEFT:
-			_skin.texture = GODOT_BOTTOM
+			_skin.texture = SPRITE_RIGHT
 		Vector2.UP:
-			_skin.texture = GODOT_BOTTOM_RIGHT
+			_skin.texture = SPRITE_UP
 		Vector2.DOWN:
-			_skin.texture = GODOT_RIGHT
+			_skin.texture = SPRITE_DOWN
 		UP_RIGHT, UP_LEFT:
-			_skin.texture = GODOT_UP
+			_skin.texture = SPRITE_UP_RIGHT
 		DOWN_RIGHT, DOWN_LEFT:
-			_skin.texture = GODOT_UP_RIGHT
-
+			_skin.texture = SPRITE_DOWN_RIGHT
 	if direction_discrete.length() > 0:
-		_skin.flip_h = direction.x < 0.0
-
+		_skin.flip_h = direction_discrete.x < 0.0
 	move_and_slide()
